d5590a819077c0144ff831e80e5a11dff94faa77
Alex Bechanko <alexbechanko@gmail.com>
2023-06-15 14:53:50 -0400

    fix odin config

--- flake.nix
+++ flake.nix
@@ -82,6 +82,7 @@
         pkgs = nixpkgs.legacyPackages.x86_64-linux;
         extraSpecialArgs = { inherit inputs; };
         modules = [ ./home/odin.nix ];
+      };
     };
 
     inherit home-manager;
