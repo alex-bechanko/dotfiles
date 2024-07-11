1f585cf36b97885556a7043761c09292ca94b82b
Alex Bechanko <alexbechanko@gmail.com>
2024-07-11 15:04:49 -0400

    Update license year

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,5 +1,5 @@
 -- Dotfiles and configurations for my machines.
--- Copyright (C) 2023 Alex Bechanko
+-- Copyright (C) 2024 Alex Bechanko
 --
 -- This program is free software: you can redistribute it and/or modify
 -- it under the terms of the GNU General Public License as published by
