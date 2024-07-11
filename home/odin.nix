da7d5ef89201d6e8c8be27613ca4c5e791e7de50
Alex Bechanko <alexbechanko@gmail.com>
2024-07-11 13:11:37 -0400

    update wsl host to use modular config

--- home/odin.nix
+++ home/odin.nix
@@ -15,27 +15,18 @@
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 
-{ inputs, lib, config, pkgs, ... }: {
+{ pkgs, ... }: {
   imports = [
-    # If you want to use home-manager modules from other flakes (such as nix-colors):
-    # inputs.nix-colors.homeManagerModule
-
-    # You can also split up your configuration and import pieces of it here:
-    # ./nvim.nix
-    ./programs/nvim.nix
+    ../modules/nvim.nix
+    ../modules/bash.nix
+    ../modules/git.nix
+    ../modules/direnv.nix
   ];
 
   nixpkgs = {
-    # You can add overlays here
-    overlays = [
-      # If you want to use overlays exported from other flakes:
-      # neovim-nightly-overlay.overlays.default
-    ];
-    # Configure your nixpkgs instance
+    overlays = [];
     config = {
-      # Disable if you don't want unfree packages
       allowUnfree = true;
-      # Workaround for https://github.com/nix-community/home-manager/issues/2942
       allowUnfreePredicate = (_: true);
     };
   };
@@ -45,25 +36,18 @@
   home = {
     username = "alex";
     homeDirectory = "/home/alex";
+    stateVersion = "22.11";
+    file.".cobra.yaml".source = ../config/cobra-cli/cobra.yaml;
+    packages = with pkgs; [
+      gnumake
+      (nerdfonts.override { fonts = [ "Inconsolata" ]; })
+      tree
+      ripgrep
+      unzip
+    ];
   };
 
   xdg.enable = true;
-
-  home.file.".cobra.yaml".source = ../config/cobra-cli/cobra.yaml;
-
-  home.packages = with pkgs; [
-    obsidian
-    discord
-    bitwarden
-    bitwarden-cli
-    beancount
-    tree
-    ripgrep
-    gnumake
-    unzip
-
-    (pkgs.nerdfonts.override { fonts = [ "Inconsolata" ]; })
-  ];
 
   fonts.fontconfig.enable = true;
 
@@ -71,40 +55,5 @@
   programs.firefox.enable = true;
   programs.htop.enable    = true;
 
-  programs.git = {
-    enable = true;
-    userName = "Alex Bechanko";
-    userEmail = "alexbechanko@gmail.com";
-    extraConfig = {
-      init.defaultBranch = "main";
-      core.pager = "less";
-    };
-  };
-
-
-  programs.bash = {
-    enable = true;
-    bashrcExtra = builtins.readFile ../config/bash/bashrc;
-    shellAliases = {
-      home-manager = "home-manager --flake /home/alex/Projects/github.com/alex-bechanko/dotfiles#alex@odin";
-    };
-  };
-
-  xdg.configFile.nvim = {
-    source = ../config/nvim;
-    recursive = true;
-  };
-
-  programs.direnv = {
-    enable = true;
-    enableBashIntegration = true;
-    nix-direnv.enable = true;
-  };
-
-
-  # Nicely reload system units when changing configs
   systemd.user.startServices = "sd-switch";
-
-  # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
-  home.stateVersion = "22.11";
 }
