4e032dbf4bf03b2a0996396357b196d0f0244538
Alex Bechanko <alexbechanko@gmail.com>
2024-08-10 13:28:44 -0400

    Update tyr to latest linux kernle

--- system/tyr.nix
+++ system/tyr.nix
@@ -13,6 +13,7 @@
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+  boot.kernelPackages = pkgs.linuxPackages_latest;
 
   networking.hostName = "tyr"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
