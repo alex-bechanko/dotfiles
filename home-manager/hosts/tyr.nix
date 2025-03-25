ac32257b211472227b76e33350f2df615f029e78
Alex Bechanko <alexbechanko@gmail.com>
2025-03-25 16:31:47 -0400

    add basic descriptions of packages in tyr

--- home-manager/hosts/tyr.nix
+++ home-manager/hosts/tyr.nix
@@ -32,26 +32,26 @@
     stateVersion = "22.11";
     file.".cobra.yaml".source = ../../config/cobra-cli/cobra.yaml;
     packages = with pkgs; [
-      bat
-      bc
-      bind
-      pkgs-stable.bitwarden
-      cargo-flamegraph
-      conform
-      discord
-      fd
-      imagemagick
-      jujutsu
-      jq
-      nerd-fonts.inconsolata
-      nerd-fonts.iosevka
-      nerd-fonts.iosevka-term
-      obsidian
-      ripgrep
-      tree
-      unzip
-      nodePackages.prettier
-      zip
+      bat # nice alternative to `cat`
+      bc # basic cli calculator
+      bind # domain name server
+      pkgs-stable.bitwarden # password manager
+      cargo-flamegraph # binary debugging tool
+      conform # code license management tool
+      discord # messaging tool
+      fd # nice alternative to `find`
+      imagemagick # cli tooling for image manipulation
+      jujutsu # git alternative
+      jq # cli tool for json querying
+      nerd-fonts.inconsolata # font
+      nerd-fonts.iosevka # font
+      nerd-fonts.iosevka-term # font
+      obsidian # note taking software
+      ripgrep # nice alternative to `grep`
+      tree # pretty display of directory contents in a tree structure
+      unzip # cli tool for unzipping .zip archives
+      nodePackages.prettier # code formatter
+      zip # cli tool for creating .zip archives
     ];
   };
 
