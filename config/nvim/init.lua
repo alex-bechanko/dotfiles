0cad799e5cabc5b6ac036095b011c231a1010ead
Alex Bechanko <alexbechanko@gmail.com>
2025-07-22 15:08:46 -0400

    Migrate neovim config to entirely in lazy.nvim

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,193 +1,71 @@
--- Dotfiles and configurations for my machines.
--- Copyright (C) 2024 Alex Bechanko
---
--- This program is free software: you can redistribute it and/or modify
--- it under the terms of the GNU General Public License as published by
--- the Free Software Foundation, either version 3 of the License, or
--- (at your option) any later version.
---
--- This program is distributed in the hope that it will be useful,
--- but WITHOUT ANY WARRANTY; without even the implied warranty of
--- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
--- GNU General Public License for more details.
---
--- You should have received a copy of the GNU General Public License
--- along with this program.  If not, see <https://www.gnu.org/licenses/>.
-
-local catppuccin = require('catppuccin')
-local nvim_treesitter_configs = require('nvim-treesitter.configs')
-local cmp = require('cmp')
-local luasnip = require('luasnip')
-local mini_indentscope = require('mini.indentscope')
-local codecompanion = require('codecompanion')
-local ufo = require('ufo')
-
-
--- colorscheme
-catppuccin.setup({
-  flavour = 'mocha',
-  styles = {
-    keywords = { 'bold' }
-  },
-  integrations = {
-    cmp = true,
-  },
-})
-vim.cmd.colorscheme('catppuccin')
-
-nvim_treesitter_configs.setup({
-  auto_install = false,
-  highlight = {
-    enable = true,
-  },
-  additional_vim_regex_highlighting = false,
-})
-
-vim.go.completeopt = 'menu,menuone,noselect'
-
--- markers to show indentation levels
-mini_indentscope.setup()
-
--- make diff mode always open in vertical split
--- the other options being set are the defaults
--- it's a pain that there isn't a way to append an option
-vim.go.diffopt = 'internal,filler,closeoff,vertical'
-
 vim.g.mapleader = [[ ]]
 vim.g.maplocalleader = [[\]]
 
+-- show absolute line number on current line
+-- show relative numbers everywhere else
 vim.wo.number = true
 vim.wo.relativenumber = true
 
+-- when splitting pane, default to the right and below
 vim.o.splitright = true
 vim.o.splitbelow = true
 
-vim.wo.signcolumn = 'yes'
+-- always show sign (warnings, errors, etc.) column
+vim.wo.signcolumn = "yes"
 
+-- don't wrap text virtually
 vim.wo.wrap = false
+-- wrap text literally at 120 characters
 vim.bo.textwidth = 120
-vim.wo.colorcolumn = '-2'
+-- show a column right before the 120th character
+vim.wo.colorcolumn = "-2"
 
 -- highlight matching braces
 vim.o.showmatch = true
 
+-- highlight cursor's line and column
 vim.wo.cursorline = true
+vim.wo.cursorcolumn = true
+
+-- show (row, columnn) in bottom right
 vim.o.ruler = true
 
+-- highlight incrementally when searching
 vim.o.incsearch = true
+
+-- case insensitive search
+vim.o.ignorecase = true
+
+-- ... unless if a capital letter is used in the search
+vim.o.smartcase = true
+
+-- set timeouts to a comfy level
 vim.o.timeout = true
 vim.o.timeoutlen = 500
-vim.o.updatetime = 100
+vim.o.updatecount = 100
 
+-- recommended settings for folds from nvim-ufo
+vim.o.foldcolumn = "1"
+vim.o.foldlevel = 99
+vim.o.foldlevelstart = 99
+vim.o.foldenable = true
+
+-- default to 4 spaces for indentation
 vim.bo.tabstop = 4
 vim.bo.softtabstop = -1
 vim.bo.shiftwidth = 4
 vim.bo.expandtab = true
 
+-- automatically indent to same level as previous line
 vim.bo.autoindent = true
+
+-- when tab is pressed use the level set for tabstops/shiftwidth
 vim.o.smarttab = true
 
--- enable case-insensitive search
-vim.o.ignorecase = true
--- ... unless if we don't want that and put a capital letter in the search
-vim.o.smartcase = true
-
--- case insensitive auto completion
-vim.o.wildignorecase = true
-
-
--- use visual (not audio) bell
+-- no beeping
 vim.o.visualbell = true
 
--- enable project specific .nvim.lua
+-- enable project specific .nvim.lua settings
 vim.o.exrc = true
 
-vim.o.foldcolumn = '0'
-vim.o.fillchars = [[eob: ,fold: ,foldopen:,foldsep: ,foldclose:]]
-vim.o.foldlevel = 99
-vim.o.foldlevelstart = 99
-vim.o.foldenable = true
-
-vim.keymap.set('n', 'zR', ufo.openAllFolds)
-vim.keymap.set('n', 'zM', ufo.closeAllFolds)
-
-ufo.setup({
-  provider_selector = function()
-    return { 'treesitter', 'indent' }
-  end
-})
-
-
--- LSP
-vim.lsp.enable('dhall_lsp_server')
-vim.lsp.enable('gopls')
-vim.lsp.enable('lua_ls')
-vim.lsp.enable('nixd')
-vim.lsp.enable('rust-analyzer')
-
-vim.api.nvim_create_autocmd('LspAttach', {
-  group = vim.api.nvim_create_augroup('UserLspConfig', {}),
-  callback = function(args)
-    local client = vim.lsp.get_client_by_id(args.data.client_id)
-    if not client then return end
-
-    vim.keymap.set('n', 'grf', vim.lsp.buf.format, { buffer = args.buf, desc = 'vim.lsp.buf.format' })
-  end
-})
-
--- AUTOCOMPLETE
-cmp.setup({
-  snippet = {
-    expand = function(args) luasnip.lsp_expand(args.body) end,
-  },
-  mapping = cmp.mapping.preset.insert({
-    ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-    ['<C-f>'] = cmp.mapping.scroll_docs(4),
-    ['<C-Space>'] = cmp.mapping.complete(),
-    ['<C-e>'] = cmp.mapping.abort(),
-    ['<CR>'] = cmp.mapping(function(fallback)
-      if cmp.visible() and luasnip.expandable() then
-        luasnip.expand()
-      elseif cmp.visible() then
-        cmp.confirm({ select = true })
-      else
-        fallback()
-      end
-    end),
-    ['<Tab>'] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_next_item()
-      elseif luasnip.locally_jumpable() then
-        luasnip.jump(1)
-      else
-        fallback()
-      end
-    end),
-    ['<S-Tab>'] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_prev_item()
-      elseif luasnip.locally_jumpable(-1) then
-        luasnip.jump(-1)
-      else
-        fallback()
-      end
-    end)
-  }),
-  sources = cmp.config.sources({
-    { name = 'nvim_lsp' },
-    { name = 'luasnip' },
-  }, { { name = 'buffer' } }),
-})
-
-
--- AI
-codecompanion.setup({
-  strategies = {
-    chat = {
-      adapter = "gemini",
-    },
-    inline = {
-      adapter = "gemini"
-    },
-  },
-})
+require('config.lazy')
