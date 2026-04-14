a68eed7c7311dfc952586c7df8929f380aff25d3
Alex Bechanko <alexbechanko@gmail.com>
2026-04-14 02:38:52 -0400

    update periodic-note to set current working directory for neovim to notes folder

--- scripts/periodic-note/periodic-note.sh
+++ scripts/periodic-note/periodic-note.sh
@@ -1,5 +1,20 @@
 
-PERIODIC_NOTES_FOLDER="$HOME/Documents/notes/areas/periodic/"
+
+NOTES_FOLDER="$HOME/Documents/notes"
+PERIODIC_NOTES_FOLDER="areas/periodic/"
+DAY_FOLDER="daily"
+WEEK_FOLDER="weekly"
+MONTH_FOLDER="monthly"
+
+_open_note() {
+    local DIRECTORY="$1"
+    local NOTE="$2"
+    mkdir -p "$NOTES_FOLDER/$PERIODIC_NOTES_FOLDER/$DIRECTORY"
+    pushd "$NOTES_FOLDER" > /dev/null
+    nvim "$PERIODIC_NOTES_FOLDER/$DIRECTORY/$NOTE"
+    popd > /dev/null
+}
+
 
 USAGE="Usage: $0 {day|week|month|-h|--help}"
 if [ -z "${1:-}" ]; then
@@ -20,22 +35,17 @@ case "$1" in
 		;;
   
 	day)
-		DAY_FOLDER="daily"
 		NOTE="$(date +%F%a).md"
-        mkdir -p "$PERIODIC_NOTES_FOLDER/$DAY_FOLDER"
-		nvim "$PERIODIC_NOTES_FOLDER/$DAY_FOLDER/$NOTE"
+        _open_note "$DAY_FOLDER" "$NOTE"
 		;;
 	week)
-		WEEK_FOLDER="weekly"
 		NOTE="$(date -d "next Monday - 7 days" "+%F").md"
-        mkdir -p "$PERIODIC_NOTES_FOLDER/$WEEK_FOLDER"
-		nvim "$PERIODIC_NOTES_FOLDER/$WEEK_FOLDER/$NOTE"
+        _open_note "$WEEK_FOLDER" "$NOTE"
 		;;
 	month)
 		MONTH_FOLDER="monthly"
 		NOTE="$(date "+%Y-%m").md"
-        mkdir -p "$PERIODIC_NOTES_FOLDER/$MONTH_FOLDER"
-		nvim "$PERIODIC_NOTES_FOLDER/$MONTH_FOLDER/$NOTE"
+        _open_note "$MONTH_FOLDER" "$NOTE"
 		;;
 	*)
 		echo "Error: Invalid option '$1'"
