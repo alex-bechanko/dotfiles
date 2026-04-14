d671578e0daeb9b1e618885bd25f0fc46ae02a64
Alex Bechanko <alexbechanko@gmail.com>
2026-04-14 03:16:42 -0400

    add zellij shortcut for daily periodic note

--- home-manager/modules/zellij.nix
+++ home-manager/modules/zellij.nix
@@ -9,14 +9,24 @@
     };
     extraConfig = ''
       keybinds {
-        normal {
-          unbind "Ctrl q"  
-        }
-        session {
-          bind "Ctrl q" {
-            Quit
+      normal {
+        unbind "Ctrl q"  
+        bind "Alt s" {
+          Run "periodic-note" "day" {
+            floating true
+            x "0"
+            y "0"
+            width "100%"
+            height "50%"
+            close_on_exit true
           }
         }
+      }
+      session {
+        bind "Ctrl q" {
+          Quit
+        }
+      }
       }
     '';
   };
